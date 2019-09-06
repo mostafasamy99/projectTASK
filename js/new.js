6894cee8ca646e1e2619fec91e2cc6a01f9887ad
mostafasamy99 <mostafamenshawy1999@gmail.com>
2019-09-06 14:37:42 +0200

    mostafa

---
+++ js/new.js
@@ -0,0 +1,205 @@
+
+  document.getElementById("rasFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("ras").style.display="block" 
+       document.getElementById("Ayon").style.display="none"
+       document.getElementById("anef").style.display="none"
+       document.getElementById("sader").style.display="none" 
+       document.getElementById("zera3").style.display="none"
+       document.getElementById("batn").style.display="none" 
+       document.getElementById("regl").style.display="none" 
+      
+})
+
+  document.getElementById("ayonFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("Ayon").style.display="block" 
+       
+       document.getElementById("ras").style.display="none" 
+       document.getElementById("anef").style.display="none"
+       document.getElementById("sader").style.display="none" 
+       document.getElementById("zera3").style.display="none"
+       document.getElementById("batn").style.display="none" 
+       document.getElementById("regl").style.display="none" 
+    
+})
+
+  document.getElementById("anefFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("anef").style.display="block" 
+       
+       document.getElementById("ras").style.display="none" 
+       document.getElementById("Ayon").style.display="none"
+       document.getElementById("sader").style.display="none" 
+       document.getElementById("zera3").style.display="none"
+       document.getElementById("batn").style.display="none" 
+       document.getElementById("regl").style.display="none" 
+    
+})
+  document.getElementById("sedrFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("sader").style.display="block" 
+       
+      document.getElementById("ras").style.display="none" 
+       document.getElementById("Ayon").style.display="none"
+       document.getElementById("anef").style.display="none" 
+       document.getElementById("zera3").style.display="none"
+       document.getElementById("batn").style.display="none" 
+       document.getElementById("regl").style.display="none" 
+    
+})
+  document.getElementById("zeraFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("zera3").style.display="block"
+       
+       document.getElementById("ras").style.display="none" 
+       document.getElementById("Ayon").style.display="none"
+       document.getElementById("anef").style.display="none"
+       document.getElementById("sader").style.display="none" 
+       document.getElementById("batn").style.display="none" 
+       document.getElementById("regl").style.display="none" 
+    
+})
+  document.getElementById("batnFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("batn").style.display="block" 
+       
+      document.getElementById("ras").style.display="none" 
+       document.getElementById("Ayon").style.display="none"
+       document.getElementById("anef").style.display="none"
+       document.getElementById("sader").style.display="none" 
+       document.getElementById("zera3").style.display="none" 
+       document.getElementById("regl").style.display="none" 
+    
+})
+document.getElementById("saeqFUN").addEventListener("click",function(){
+    
+    
+    document.getElementById("regl").style.display="block" 
+       
+    document.getElementById("ras").style.display="none" 
+       document.getElementById("Ayon").style.display="none"
+       document.getElementById("anef").style.display="none"
+       document.getElementById("sader").style.display="none" 
+       document.getElementById("zera3").style.display="none"
+       document.getElementById("batn").style.display="none"  
+    
+})
+
+///نهاية الراجل
+
+
+
+
+
+
+document.getElementById("rasFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("rasSET").style.display="block" 
+       document.getElementById("AyonSET").style.display="none"
+       document.getElementById("anefSET").style.display="none"
+       document.getElementById("saderSET").style.display="none" 
+       document.getElementById("zera3SET").style.display="none"
+       document.getElementById("batnSET").style.display="none" 
+       document.getElementById("reglSET").style.display="none" 
+      
+})
+
+  document.getElementById("ayonFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("AyonSET").style.display="block" 
+       
+       document.getElementById("rasSET").style.display="none" 
+       document.getElementById("anefSET").style.display="none"
+       document.getElementById("saderSET").style.display="none" 
+       document.getElementById("zera3SET").style.display="none"
+       document.getElementById("batnSET").style.display="none" 
+       document.getElementById("reglSET").style.display="none" 
+    
+})
+  document.getElementById("anefFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("anefSET").style.display="block" 
+       
+       document.getElementById("rasSET").style.display="none" 
+       document.getElementById("AyonSET").style.display="none"
+       document.getElementById("saderSET").style.display="none" 
+       document.getElementById("zera3SET").style.display="none"
+       document.getElementById("batnSET").style.display="none" 
+       document.getElementById("reglSET").style.display="none" 
+    
+})
+  document.getElementById("sedrFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("saderSET").style.display="block" 
+       
+      document.getElementById("rasSET").style.display="none" 
+       document.getElementById("AyonSET").style.display="none"
+       document.getElementById("anefSET").style.display="none" 
+       document.getElementById("zera3SET").style.display="none"
+       document.getElementById("batnSET").style.display="none" 
+       document.getElementById("reglSET").style.display="none" 
+    
+})
+  document.getElementById("zeraFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("zera3SET").style.display="block"
+       
+       document.getElementById("rasSET").style.display="none" 
+       document.getElementById("AyonSET").style.display="none"
+       document.getElementById("anefSET").style.display="none"
+       document.getElementById("saderSET").style.display="none" 
+       document.getElementById("batnSET").style.display="none" 
+       document.getElementById("reglSET").style.display="none" 
+    
+})
+  document.getElementById("batnFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("batnSET").style.display="block" 
+       
+      document.getElementById("rasSET").style.display="none" 
+       document.getElementById("AyonSET").style.display="none"
+       document.getElementById("anefSET").style.display="none"
+       document.getElementById("saderSET").style.display="none" 
+       document.getElementById("zera3SET").style.display="none" 
+       document.getElementById("reglSET").style.display="none" 
+    
+})
+document.getElementById("saeqFUNSET").addEventListener("click",function(){
+    
+    
+    document.getElementById("reglSET").style.display="block" 
+       
+    document.getElementById("rasSET").style.display="none" 
+       document.getElementById("AyonSET").style.display="none"
+       document.getElementById("anefSET").style.display="none"
+       document.getElementById("saderSET").style.display="none" 
+       document.getElementById("zera3SET").style.display="none"
+       document.getElementById("batnSET").style.display="none"  
+    
+})
+
+
+//نهاية الست
+
+//
+
+
+
+
+
+
+
+
