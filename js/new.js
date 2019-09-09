a57de8b8f16e2797d76caf3ae51307d0aaa1e669
mostafasamy99 <mostafamenshawy1999@gmail.com>
2019-09-09 22:33:21 +0200

    last update

---
+++ js/new.js
@@ -0,0 +1,618 @@
+document.getElementById("rasFUN").addEventListener("click", function () {
+
+
+  document.getElementById("ras").style.display = "block";
+  document.getElementById("Ayon").style.display = "none";
+  document.getElementById("anef").style.display = "none";
+  document.getElementById("sader").style.display = "none";
+  document.getElementById("zera3").style.display = "none";
+  document.getElementById("batn").style.display = "none";
+  document.getElementById("regl").style.display = "none";
+
+})
+
+document.getElementById("ayonFUN").addEventListener("click", function () {
+
+
+  document.getElementById("Ayon").style.display = "block";
+
+  document.getElementById("ras").style.display = "none";
+  document.getElementById("anef").style.display = "none";
+  document.getElementById("sader").style.display = "none";
+  document.getElementById("zera3").style.display = "none";
+  document.getElementById("batn").style.display = "none";
+  document.getElementById("regl").style.display = "none";
+
+})
+
+document.getElementById("anefFUN").addEventListener("click", function () {
+
+
+  document.getElementById("anef").style.display = "block";
+
+  document.getElementById("ras").style.display = "none";
+  document.getElementById("Ayon").style.display = "none";
+  document.getElementById("sader").style.display = "none";
+  document.getElementById("zera3").style.display = "none";
+  document.getElementById("batn").style.display = "none";
+  document.getElementById("regl").style.display = "none";
+
+})
+document.getElementById("sedrFUN").addEventListener("click", function () {
+
+
+  document.getElementById("sader").style.display = "block";
+
+  document.getElementById("ras").style.display = "none";
+  document.getElementById("Ayon").style.display = "none";
+  document.getElementById("anef").style.display = "none";
+  document.getElementById("zera3").style.display = "none";
+  document.getElementById("batn").style.display = "none";
+  document.getElementById("regl").style.display = "none";
+
+})
+document.getElementById("zeraFUN").addEventListener("click", function () {
+
+
+  document.getElementById("zera3").style.display = "block";
+
+  document.getElementById("ras").style.display = "none";
+  document.getElementById("Ayon").style.display = "none";
+  document.getElementById("anef").style.display = "none";
+  document.getElementById("sader").style.display = "none";
+  document.getElementById("batn").style.display = "none";
+  document.getElementById("regl").style.display = "none";
+
+})
+document.getElementById("batnFUN").addEventListener("click", function () {
+
+
+  document.getElementById("batn").style.display = "block";
+
+  document.getElementById("ras").style.display = "none";
+  document.getElementById("Ayon").style.display = "none";
+  document.getElementById("anef").style.display = "none";
+  document.getElementById("sader").style.display = "none";
+  document.getElementById("zera3").style.display = "none";
+  document.getElementById("regl").style.display = "none";
+
+})
+document.getElementById("saeqFUN").addEventListener("click", function () {
+
+
+  document.getElementById("regl").style.display = "block";
+
+  document.getElementById("ras").style.display = "none";
+  document.getElementById("Ayon").style.display = "none";
+  document.getElementById("anef").style.display = "none";
+  document.getElementById("sader").style.display = "none";
+  document.getElementById("zera3").style.display = "none";
+  document.getElementById("batn").style.display = "none";
+
+})
+
+///نهاية الراجل
+
+
+
+
+
+
+document.getElementById("rasFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("rasSET").style.display = "block";
+  document.getElementById("AyonSET").style.display = "none";
+  document.getElementById("anefSET").style.display = "none";
+  document.getElementById("saderSET").style.display = "none";
+  document.getElementById("zera3SET").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+  document.getElementById("reglSET").style.display = "none";
+
+})
+
+document.getElementById("ayonFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("AyonSET").style.display = "block";
+
+  document.getElementById("rasSET").style.display = "none";
+  document.getElementById("anefSET").style.display = "none";
+  document.getElementById("saderSET").style.display = "none";
+  document.getElementById("zera3SET").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+  document.getElementById("reglSET").style.display = "none";
+
+})
+document.getElementById("anefFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("anefSET").style.display = "block";
+
+  document.getElementById("rasSET").style.display = "none";
+  document.getElementById("AyonSET").style.display = "none";
+  document.getElementById("saderSET").style.display = "none";
+  document.getElementById("zera3SET").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+  document.getElementById("reglSET").style.display = "none";
+
+})
+document.getElementById("sedrFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("saderSET").style.display = "block";
+
+  document.getElementById("rasSET").style.display = "none";
+  document.getElementById("AyonSET").style.display = "none";
+  document.getElementById("anefSET").style.display = "none";
+  document.getElementById("zera3SET").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+  document.getElementById("reglSET").style.display = "none";
+
+})
+document.getElementById("zeraFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("zera3SET").style.display = "block";
+
+  document.getElementById("rasSET").style.display = "none";
+  document.getElementById("AyonSET").style.display = "none";
+  document.getElementById("anefSET").style.display = "none";
+  document.getElementById("saderSET").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+  document.getElementById("reglSET").style.display = "none";
+
+})
+document.getElementById("batnFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("batnSET").style.display = "block";
+
+  document.getElementById("rasSET").style.display = "none";
+  document.getElementById("AyonSET").style.display = "none";
+  document.getElementById("anefSET").style.display = "none";
+  document.getElementById("saderSET").style.display = "none";
+  document.getElementById("zera3SET").style.display = "none";
+  document.getElementById("reglSET").style.display = "none";
+
+})
+document.getElementById("saeqFUNSET").addEventListener("click", function () {
+
+
+  document.getElementById("reglSET").style.display = "block";
+
+  document.getElementById("rasSET").style.display = "none";
+  document.getElementById("AyonSET").style.display = "none";
+  document.getElementById("anefSET").style.display = "none";
+  document.getElementById("saderSET").style.display = "none";
+  document.getElementById("zera3SET").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+
+})
+
+
+//نهاية الست
+
+//الطفل
+
+document.getElementById("rasFUNBABY").addEventListener("click", function () {
+
+
+  document.getElementById("rasBABY").style.display = "block";
+  document.getElementById("AyonBABY").style.display = "none";
+  document.getElementById("anefBABY").style.display = "none";
+  document.getElementById("saderBABY").style.display = "none";
+  //document.getElementById("zera3BABY").style.display = "none";
+  document.getElementById("batnBABY").style.display = "none";
+  //document.getElementById("reglBABY").style.display = "none";
+
+})
+
+document.getElementById("ayonFUNBABY").addEventListener("click", function () {
+
+
+  document.getElementById("AyonBABY").style.display = "block";
+
+  document.getElementById("rasBABY").style.display = "none";
+  document.getElementById("anefBABY").style.display = "none";
+  document.getElementById("saderBABY").style.display = "none";
+  //document.getElementById("zera3BABY").style.display = "none";
+  document.getElementById("batnBABY").style.display = "none";
+  //document.getElementById("reglBABY").style.display = "none";
+
+})
+document.getElementById("anefFUNBABY").addEventListener("click", function () {
+
+
+  document.getElementById("anefBABY").style.display = "block";
+
+  document.getElementById("rasBABY").style.display = "none";
+  document.getElementById("AyonBABY").style.display = "none";
+  document.getElementById("saderBABY").style.display = "none";
+  //document.getElementById("zera3BABY").style.display = "none";
+  document.getElementById("batnBABY").style.display = "none";
+  //document.getElementById("reglBABY").style.display = "none";
+
+})
+document.getElementById("sedrFUNBABY").addEventListener("click", function () {
+
+
+  document.getElementById("saderBABY").style.display = "block";
+
+  document.getElementById("rasBABY").style.display = "none";
+  document.getElementById("AyonBABY").style.display = "none";
+  document.getElementById("anefBABY").style.display = "none";
+ // document.getElementById("zera3BABY").style.display = "none";
+  document.getElementById("batnSET").style.display = "none";
+  //document.getElementById("reglBABY").style.display = "none";
+
+})
+
+//document.getElementById("zeraFUNBABY").addEventListener("click", function () {
+//
+//
+//  document.getElementById("zera3BABY").style.display = "block";
+//
+//  document.getElementById("rasBABY").style.display = "none";
+//  document.getElementById("AyonBABY").style.display = "none";
+//  document.getElementById("anefBABY").style.display = "none";
+//  document.getElementById("saderBABY").style.display = "none";
+//  document.getElementById("batnBABY").style.display = "none";
+//  document.getElementById("reglBABY").style.display = "none";
+//
+//})
+document.getElementById("batnFUNBABY").addEventListener("click", function () {
+
+
+  document.getElementById("batnBABY").style.display = "block";
+
+  document.getElementById("rasBABY").style.display = "none";
+  document.getElementById("AyonBABY").style.display = "none";
+  document.getElementById("anefBABY").style.display = "none";
+  document.getElementById("saderBABY").style.display = "none";
+  //document.getElementById("zera3BABY").style.display = "none";
+  //document.getElementById("reglBABY").style.display = "none";
+
+})
+//document.getElementById("saeqFUNBABY").addEventListener("click", function () {
+//
+//
+//  document.getElementById("reglBABY").style.display = "block";
+//
+//  document.getElementById("rasBABY").style.display = "none";
+//  document.getElementById("AyonBABY").style.display = "none";
+//  document.getElementById("anefBABY").style.display = "none";
+//  document.getElementById("saderBABY").style.display = "none";
+//  document.getElementById("zera3BABY").style.display = "none";
+//  document.getElementById("batnBABY").style.display = "none";
+//
+//})
+
+
+
+//
+
+// m7md //
+// miniHome , miniDrugs , miniDesases , healthBeauty , medicalHealth -->> id bto3 el navBar
+
+// upperNAV ,bottomNAV ,HOME , DRUGS,diseases , ELRAGEL ,WOMANsymptom -->> id bto3 el sections
+
+// unHomme رجل , leFemme المراه , leEnfant الطفل -->> man,woman,child -id-
+
+
+$("#unHomme").click(function(){
+
+
+  $("#ELRAGEL").css("display","block");
+
+  $("#diseases").css("display","none");
+  $("#HOME").css("display","none");
+  $("#DRUGS").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+ $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+
+});
+
+$("#leFemme").click(function(){
+
+  $("#WOMANsymptom").css("display","block");
+
+  $("#ELRAGEL").css("display","none");
+  $("#diseases").css("display","none");
+  $("#HOME").css("display","none");
+  $("#DRUGS").css("display","none");
+  $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+});
+$("#leEnfant").click(function(){
+
+  $("#ELBABY").css("display","block");
+
+ $("#WOMANsymptom").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#diseases").css("display","none");
+  $("#HOME").css("display","none");
+  $("#DRUGS").css("display","none");
+  $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+});
+
+// end of small buttons which refers to man woman child
+
+
+$("#miniDesases").click(function(){
+  
+  $("#diseases").css("display","block");
+
+  $("#HOME").css("display","none");
+  $("#DRUGS").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+  $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+    
+});
+
+$("#miniDrugs").click(function(){
+  
+  $("#DRUGS").css("display","block");
+
+  $("#HOME").css("display","none");
+  $("#diseases").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+ $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+});
+
+$("#miniHome").click(function(){
+
+  $("#HOME").css("display","block");
+
+  $("#DRUGS").css("display","none");
+  $("#diseases").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+ $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+});
+
+
+// default block pages when load home
+$("#HOME").css("display","block");
+
+$("#DRUGS").css("display","none");
+$("#diseases").css("display","none");
+$("#ELRAGEL").css("display","none");
+$("#WOMANsymptom").css("display","none");
+ $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+ $("#ELBABY").css("display","none");
+////////////////////////////////////////////
+
+
+
+$("#healthBeauty").click(function(){
+    $("#HealthBeautY").css("display","block");
+    
+    
+  $("#DRUGS").css("display","none");
+  $("#HOME").css("display","none");
+  $("#diseases").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+    
+});
+
+
+
+$("#medicalHealth").click(function(){
+    
+    $("#MedicalHealth").css("display","block");
+    
+    
+    
+    
+  $("#DRUGS").css("display","none");
+  $("#HOME").css("display","none");
+  $("#diseases").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+   $("#HealthBeautY").css("display","none");
+  $("#signUP").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+    
+});
+
+
+$("#miniSIGNUP").click(function(){
+    
+    $("#signUP").css("display","block");
+    
+    
+    
+    
+  $("#DRUGS").css("display","none");
+  $("#HOME").css("display","none");
+  $("#diseases").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+   $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signIN").css("display","none");
+  $("#ELBABY").css("display","none");
+    
+});
+
+$("#miniSIGNIN").click(function(){
+    
+    $("#signIN").css("display","block");
+    
+    
+    
+    
+  $("#DRUGS").css("display","none");
+  $("#HOME").css("display","none");
+  $("#diseases").css("display","none");
+  $("#ELRAGEL").css("display","none");
+  $("#WOMANsymptom").css("display","none");
+   $("#HealthBeautY").css("display","none");
+  $("#MedicalHealth").css("display","none");
+  $("#signUP").css("display","none");
+  $("#ELBABY").css("display","none");
+    
+});
+
+
+
+
+
+///////////////////////////////////////////////
+
+
+// navBar FIXED TOP
+
+// let naving = $(".bottmNav").offset().top;
+
+// $(window).scroll(function(){
+
+//   let scrollTop = $(window).scrollTop();
+ 
+  
+//   if(scrollTop > naving)
+//   {
+//     $(".upperNav").css("display","none");
+//     $(".bottmNav").css("position","fixed").css("backgroundColor","rgba(78, 125, 141,0.8)");
+//   }
+//   else
+//   {
+//    $(".upperNav").css("display","block");
+//    $(".bottmNav").css("position","relative").css("backgroundColor","rgba(78, 125, 141,1)");
+//   }
+ 
+ 
+//  });
+
+
+
+
+
+// signUP data
+var firstName = document.getElementById("firstName");
+var lastName = document.getElementById("lastName");
+var userEmail = document.getElementById("userEmail");
+var userPassword = document.getElementById("userPassword");
+var rePassword = document.getElementById("rePassword");
+
+var signInBtn = document.getElementById("signInBtn");
+//
+
+// signIN data
+
+var loginEmail = document.getElementById("loginEmail");
+var loginPassword = document.getElementById("loginPassword");
+
+var loginBtn = document.getElementById("loginBtn");
+//
+
+var loginValidation = JSON.parse(localStorage.getItem("userData"));
+
+var newArray;
+if(localStorage.getItem("userData")== null)
+{
+  newArray = [];
+}
+else
+{
+    newArray = JSON.parse(localStorage.getItem("userData"));
+}
+
+loginBtn.onclick = function()
+{
+  loginValidation = JSON.parse(localStorage.getItem("userData"));
+  var booleanValid= false;
+  for(let i = 0 ; i < loginValidation.length ; i++)
+  {
+    if(loginEmail.value == loginValidation[i].useremail && loginPassword.value == loginValidation[i].userpassword)
+    {
+      alert("تم تسجيل الدخول بنجاح");
+       booleanValid=true; 
+    }
+  }
+    if(!booleanValid){
+        alert("اسم المستخدم أو كلمة المرور غير صالحة");
+    }
+        
+
+}
+
+
+
+
+signInBtn.onclick = function()
+ {
+    
+    if(firstName.value !="" && lastName.value!="" && userEmail.value!="" && userPassword.value!="" && rePassword.value!="")
+    {
+        
+  var users = 
+      {
+        fristname : firstName.value,
+        lastname  : lastName.value,
+        useremail : userEmail.value,
+        userpassword : userPassword.value,
+        repassword : rePassword.value
+      }
+      newArray.push(users);
+
+      localStorage.setItem("userData",JSON.stringify(newArray));
+        // clear();
+        alert("تم السجيل ! مرحبا بك في موقعنا الطبي")
+    }
+    else{alert("يرجي التحقق من ملئ جميع البيانات!");}
+    
+ }
+
+  
+function clear()
+{
+    firstName="";
+    lastName="";
+    userEmail="";
+    userPassword="";
+    rePassword="";
+}
+
+
+
+
+
+
+
+
+
